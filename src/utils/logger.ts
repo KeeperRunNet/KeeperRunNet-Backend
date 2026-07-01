a959f8b282d594e263d24d491fb63fa8ba1ce0c3
Eden Analytics <87858513+Emmanu-el07@users.noreply.github.com>
2026-07-01 22:54:07 +0100

    Add logger utility using Winston for logging
    - Added winston logger and logging formatting similar to spring logging.

---
+++ src/utils/logger.ts
@@ -0,0 +1,31 @@
+import winston, { createLogger, format as _format, transports as _transports } from 'winston';
+
+const logger = createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: _format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({stack:true}),
+    winston.format.printf(({ level, message, timestamp, services }) => {
+      return `[${timestamp}] [${level}] [${services}]: ${message}`
+    })
+  ),
+  defaultMeta: { service: 'authSwervice' },
+  transports: [
+    new _transports.Console({
+      format: _format.combine(
+        _format.colorize(),
+        _format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        _format.errors({ stack: true }),
+        _format.splat(),
+        _format.json(),
+        _format.printf((info) => `[${info.timestamp}] [${info.level}] [${info.services}]: ${info.message}`),
+      ),
+    }),
+    new _transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new _transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+export default logger;
